84ef329a62e299d04697175cdf4d10bd1228078e
SilveryPaw <sir.sinitzin2012@yandex.ru>
2023-03-18 20:45:21 +0300

    Renamed index.html to index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="styles/main.css" rel="stylesheet">
+    </head>
+    <body id="body">
+        <?php include_once "template/header.php"; ?> <!--Подключаем менюшку-->
+        <main id="content_block">
+            <div class="content">
+                <div id="about_block">
+                    <h1>О сайте</h1>
+                    <p>Сайт WolfArticles хранит различные статьи, а также описание и характеристики различных видов псовых. Работа проведена с целью ознакомления и закрепления навыков web-разработки</p>
+                </div>
+                <div id="recomends_block">
+                    <h1>Интересные статьи</h1>
+                </div>
+                <div id="facts_block">
+                    <h1>Случайный факт</h1>
+                </div>
+            </div>
+        </main>
+        <?php include_once "template/footer.php"; ?> <!--Подключаем подвал-->
+    </body>
+</html>
