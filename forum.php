fdebe67eeaf7899209d8702595dcd1f96bbafef3
SilveryPaw <sir.sinitzin2012@yandex.ru>
2023-03-18 20:49:04 +0300

    Added pages templates

---
+++ forum.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="styles/main.css" rel="stylesheet">
+    </head>
+    <body id="body">
+        <?php include_once "template/header.php"; ?> <!--Подключаем менюшку-->
+        <main id="content_block">
+            <div class="content">
+                <h1>Форум</h1>
+            </div>
+        </main>
+        <?php include_once "template/footer.php"; ?> <!--Подключаем подвал-->
+    </body>
+</html>
