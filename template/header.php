6ef2cda7f6bcfc1b6c6991d8d0ff635e16cb2360
SilveryPaw <sir.sinitzin2012@yandex.ru>
2023-03-19 20:12:25 +0300

    Fixed links

--- template/header.php
+++ template/header.php
@@ -4,7 +4,7 @@
             <li><a href="/wolf_articles/index.php">WolfArticles</a></li>
             <li><a href="/wolf_articles/articles.php">Статьи</a></li>
             <li><a href="/wolf_articles/canines.php">Виды псовых</a></li>
-            <li><a href="/wolf_articles/blog.php">Блог</a></li>
+            <li><a href="/wolf_articles/forum.php">Форум</a></li>
             <li><a href="/wolf_articles/contacts.php">Контакты</a></li>
         </ul>
         <div id="log_reg">
