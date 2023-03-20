5d28bf1f0d9c1729d309b5e925edfb6b9b3bf2f4
SilveryPaw <sir.sinitzin2012@yandex.ru>
2023-03-20 22:29:55 +0300

    jQuery included

--- template/meta_data.php
+++ template/meta_data.php
@@ -1,3 +1,4 @@
 <meta charset="utf-8">
 <link href="styles/main.css" rel="stylesheet">
 <link href="styles/content.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
