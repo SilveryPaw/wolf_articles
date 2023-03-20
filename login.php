11594ac152790d30fb0d57d8ff6c06c5389fb16a
SilveryPaw <sir.sinitzin2012@yandex.ru>
2023-03-20 22:27:07 +0300

    added login php file. Not working yet

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include_once "template/meta_data.php" ?><!--Подключаем метаданные(стили, кодировку)-->
+    </head>
+    <body id="body">
+        <?php include_once "template/header.php"; ?> <!--Подключаем менюшку-->
+        <main id="content_block">
+            <div class="content">
+                <h1>Авторизация</h1>
+                
+            </div>
+        </main>
+        <?php include_once "template/footer.php"; ?> <!--Подключаем подвал-->
+    </body>
+</html>
