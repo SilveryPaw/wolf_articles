702e32df3951892554ee4b8b3007431c2143af5c
SilveryPaw <sir.sinitzin2012@yandex.ru>
2023-03-23 21:01:36 +0300

    Added authorization feature

--- login.php
+++ login.php
@@ -1,14 +1,81 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <?php include_once "template/meta_data.php" ?><!--Подключаем метаданные(стили, кодировку)-->
+        <?php include_once "template/meta_data.php";?><!--Подключаем метаданные(стили, кодировку)-->
+        <script src="scripts/error_messages.js"></script>
+        <script>
+            function send_data(login, pword)
+            {
+                let xhr = new XMLHttpRequest();
+                let json = JSON.stringify({
+                    "Login": login,
+                    "Password": pword
+                });
+                xhr.open ('POST', "scripts/log_user.php");
+                xhr.setRequestHeader('Content-type', 'application/json');
+                xhr.send(json);
+
+                xhr.onload = function()
+                {
+                    if(xhr.status != 200)
+                    {
+                        alert (`Ошибка ${xhr.status}: ${statusText}`);
+                    }
+                    else
+                    {
+                        switch(xhr.response)
+                        {
+                            case "pass":
+                                window.location.href = "index.php";
+                                break;
+                            case "fail":
+                                alert ("Ошибка входных данных. Проверьте правильность логина и пароля");
+                                break;
+                        }
+                    }
+                }
+            }
+
+            function check_data()
+            {
+                clear_error_messages();
+                let iscorrect = true;
+                let warning = "Поле не должно быть пустым";
+                let login = document.getElementById("login_block");
+                let password = document.getElementById("password_block");
+                if(login.querySelector("#login").value == "")
+                {
+                    add_error_message(login, warning);
+                    iscorrect = false;
+                }
+                if(password.querySelector("#password").value == "")
+                {
+                    add_error_message(password, warning);
+                    iscorrect = false;
+                }
+                if(iscorrect)
+                {
+                    send_data(login.querySelector("#login").value, password.querySelector("#password").value);
+                }
+            }
+        </script>
     </head>
     <body id="body">
         <?php include_once "template/header.php"; ?> <!--Подключаем менюшку-->
         <main id="content_block">
-            <div class="content">
+            <form class="content">
                 <h1>Авторизация</h1>
-                
+                <div class="data_block">
+                    <div id="login_block">
+                        <label for="login">Логин</label>
+                        <input id="login" type="text" placeholder="Введите имя пользователя или e-mail"/>
+                    </div>
+                    <div id="password_block">
+                        <label for="password">Пароль</label>
+                        <input id="password" type="password" placeholder="Введите пароль"/>
+                    </div>
+                    <input id="push_data" type="button" value="Войти" onclick="check_data()"/>
+                </div>
             </div>
         </main>
         <?php include_once "template/footer.php"; ?> <!--Подключаем подвал-->
---
+++ scripts/log_user.php
@@ -0,0 +1,50 @@
+<?php 
+session_start();
+include_once 'connection.php';
+$conn = new mysqli ($host, $login, $password, $database);
+
+function is_correct()
+{
+    global $user_login, $user_pword, $conn, $_SESSION;
+
+    $sql = "SELECT `pword` FROM `users` WHERE `username` = \"$user_login\" OR `mail` = \"$user_login\"";
+    if($result = mysqli_query($conn, $sql))
+    {
+        if($result->num_rows > 0)
+        {
+            $result->data_seek(0);
+            $row = $result->fetch_assoc();
+            if(password_verify($user_pword, $row["pword"]))//Сравниваем хешированный пароль из бд с введенным паролем
+            {
+                $_SESSION["Uname"] = $user_login;
+                $_SESSION["Upword"] = $row["pword"];
+                echo "pass";
+            }
+            else
+            {
+                echo "fail";
+            }
+        }
+        else
+        {
+            echo "fail";
+        }
+    }
+}
+
+
+header("Content-Type: application/json");
+$data = json_decode(file_get_contents("php://input"));//Стандартная команда для получения данных
+$user_login = $data->Login;
+$user_pword = $data->Password;
+is_correct();
+/*if (is_correct())
+{
+    echo(true);
+}
+else
+{
+    echo("Авторизация не удалась, проверьте имя пользователя и пароль.");
+}*/
+$conn->close();
+?>
