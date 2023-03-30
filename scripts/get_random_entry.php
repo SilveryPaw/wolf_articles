c3e678289b659dc2ed2f8555ffd503706295d947
SilveryPaw <sir.sinitzin2012@yandex.ru>
2023-03-30 13:12:34 +0300

    Created function to get random entry from one of the tables

---
+++ scripts/get_random_entry.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once 'connection.php';
+$conn = new mysqli($host, $login, $password, $database);
+if(($table == "articles")||($table == "forums"))
+{
+    $sql = "SELECT *, (SELECT users.username FROM users WHERE $table.author_id = users.id) AS author_name FROM $table ORDER BY RAND() LIMIT 1";
+
+}else
+{
+    $sql = "SELECT * FROM $table ORDER BY RAND() LIMIT 1";
+}
+if($conn->connect_error)
+{
+    die("Error: ".$conn->connect_error);
+}
+if($result = mysqli_query($conn, $sql))
+{
+    $pages_count = 0;
+    switch($table){
+        case "articles":
+            include_once 'create_articles_block.php';
+            break;
+        case "forums":
+            include_once 'create_forums_block.php';
+            break;
+        case "facts":
+            include_once 'create_facts_block.php';
+            break;
+    }
+}
+?>
