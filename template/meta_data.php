5829f455fcdd3c62591e47e5f987124fd370da15
SilveryPaw <sir.sinitzin2012@yandex.ru>
2023-04-02 22:11:56 +0300

    Added adaptive design

--- template/meta_data.php
+++ template/meta_data.php
@@ -2,4 +2,5 @@
 <link href="styles/main.css" rel="stylesheet">
 <link href="styles/content.css" rel="stylesheet">
 <link href="styles/menu.css" rel="stylesheet">
+<link href="styles/adaptive.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
